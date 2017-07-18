31ead540c64d0bfbf4b7a5be68ea1003c02528c6
Kinchit Raja <kinchit15@gmail.com>
2017-07-17 21:51:16 -0700

    added php files

---
+++ My Websites/7. PHP/example.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+<head>
+    <title>My First Webpage</title>
+
+    <meta charset="utf-8" />
+    <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />    
+</head>
+
+<body>
+<div>
+	<form>
+		
+	<label for="name"></label>
+	<input type="text" name="name">	
+	<input type="submit">
+	</form>
+</div>
+</body>
+</html>
